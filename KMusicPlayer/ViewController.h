ef353dfca29a09240132c4248bfb91baa28d6c98
zxk3131 <xiangkai.zhao@careerdream.org>
2016-11-01 10:51:31 +0800

    Initial Commit

---
+++ KMusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KMusicPlayer
+//
+//  Created by 赵祥凯 on 16/11/1.
+//  Copyright © 2016年 zxk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
